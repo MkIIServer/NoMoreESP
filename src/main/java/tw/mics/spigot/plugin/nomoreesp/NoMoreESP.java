b8afdc387a05f5fd3088f05fb14baeb12f79fe4e
Mics <mics@mics.tw>
2016-06-28 04:28:51 +0800

    add plugin log function

--- src/main/java/tw/mics/spigot/plugin/nomoreesp/NoMoreESP.java
+++ src/main/java/tw/mics/spigot/plugin/nomoreesp/NoMoreESP.java
@@ -1,19 +1,25 @@
 package tw.mics.spigot.plugin.nomoreesp;
 
 import org.bukkit.plugin.java.JavaPlugin;
+
 import tw.mics.spigot.plugin.nomoreesp.schedule.ESPCheckSchedule;
 
 public class NoMoreESP extends JavaPlugin {
     private static NoMoreESP INSTANCE;
-    
+
     @Override
     public void onEnable() {
         INSTANCE = this;
         new ESPCheckSchedule(this);
     }
 
+    public void log(String str, Object... args) {
+        String message = String.format(str, args);
+        getLogger().info(message);
+    }
+
     public static NoMoreESP getInstance() {
         return INSTANCE;
     }
-    
+
 }
